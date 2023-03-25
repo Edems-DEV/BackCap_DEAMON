eb4fb7c4c0f2552e120d52de2afc907ca8d47949
CyrdaO <112561057+CyrdaO@users.noreply.github.com>
2023-03-25 19:28:35 +0100

    abstracrt

---
+++ Deamon/Backup/BackupType.cs
@@ -0,0 +1,25 @@
+﻿using Deamon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deamon.Backup;
+public abstract class BackupType
+{
+    public List<DirectoryInfo> SourceList = new List<DirectoryInfo>();
+    private protected Config Config;
+
+    protected BackupType(Config config)
+    {
+        this.Config = config;
+
+        foreach (Sources item in config.Sources)
+        {
+            DirectoryInfo Dir = new DirectoryInfo(item.Path);
+            SourceList.Add(Dir);
+        }
+    }
+    public abstract void Backup();
+}
