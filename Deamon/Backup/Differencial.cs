e8f0d64696303d7f5329bb1e05ae8984521ed121
baradam05 <120955242+baradam05@users.noreply.github.com>
2023-04-02 14:15:29 +0200

    pridani backupty trid

---
+++ Deamon/Backup/Differencial.cs
@@ -0,0 +1,24 @@
+﻿using Deamon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deamon.Backup;
+internal class Differencial : BackupType
+{
+    public Differencial(Config config) : base(config) { }
+
+    public override void UpdateSnapchot(string json, string path)
+    {
+        if (new FileInfo(path).Length == 0)
+        {
+            using (StreamWriter sr = new StreamWriter(path))
+            {
+                sr.WriteLine(json);
+            }
+        }
+        
+    }
+}
---
+++ Deamon/Backup/Inkremental.cs
@@ -0,0 +1,18 @@
+﻿using Deamon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deamon.Backup;
+internal class Inkremental : BackupType
+{
+    public Inkremental(Config config) : base(config) { }
+
+
+    public override void UpdateSnapchot(string json, string path)
+    {
+        throw new NotImplementedException();
+    }
+}
