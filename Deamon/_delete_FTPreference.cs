110a697d28f199e9be093da92e277ca174ec28b7
baradam05 <120955242+baradam05@users.noreply.github.com>
2023-06-02 22:07:38 +0200

    Přídal jsem FTP referenci (jen pro mě abych to nemusel mít na internetu/v jiném projektu) *chápu jak to funguje *celé jsem to okomentoval
    (jsem moc unavenej abych to dneska už načal. Ačkoliv to nemá patu, hlavu to už má)

---
+++ Deamon/_delete_FTPreference.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deamon;
+internal class _delete_FTPreference
+{
+    public void FTPreference()
+    {
+        string server = "ftp://example.com";
+        string username = "your-username";
+        string password = "your-password";
+
+        // Connect to the FTP server
+        FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create(server); //vytvoří request
+        ftpRequest.Credentials = new NetworkCredential(username, password);   //přihlášení pomocí údajů
+        ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;              //???
+
+        FtpWebResponse ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();//získání odpovědi od FTP
+        Stream responseStream = ftpResponse.GetResponseStream();              //do třídy Strem se přečte ten response
+        StreamReader reader = new StreamReader(responseStream);               //streamreader to začne číst
+
+        // List directory contents
+        Console.WriteLine("Directory List:");
+        Console.WriteLine(reader.ReadToEnd());                                //vypíše co přečetl
+
+        string localFilePath = "C:\\path\\to\\local\\file.txt";               //to co budeme kopírovat
+        string remoteFilePath = "/path/to/remote/file.txt";                   //to kam to budeme kopírovat (mi to budeme mít už rovnou za servererm)
+
+        // ------------------------------------------------------------------- UPLOAD FILE ----------------------------------------------------
+        ftpRequest = (FtpWebRequest)WebRequest.Create(server + remoteFilePath);//vytvoří request
+        ftpRequest.Credentials = new NetworkCredential(username, password);   //přihlášení pomocí údajů
+        ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;                 //???
+
+        byte[] fileContents = File.ReadAllBytes(localFilePath);               //zjistí to guts toho filu
+        ftpRequest.ContentLength = fileContents.Length;                       //+ délku
+
+        Stream requestStream = ftpRequest.GetRequestStream();                 //uloží si ftp Stream
+        requestStream.Write(fileContents, 0, fileContents.Length);            //do toho zapíše guts toho filu (data, offset (0), délka)
+        requestStream.Close();                                                //uzavře
+
+        FtpWebResponse uploadResponse = (FtpWebResponse)ftpRequest.GetResponse();//NAHRAJE DATA Z REQUESTU
+        Console.WriteLine("Upload File Complete. Status: " + uploadResponse.StatusDescription);
+
+        // ------------------------------------------------------------------- DELETE FILE ----------------------------------------------------
+        ftpRequest = (FtpWebRequest)WebRequest.Create(server + remoteFilePath);//vytvoří request
+        ftpRequest.Credentials = new NetworkCredential(username, password);   //přihlášení pomocí údajů 
+        ftpRequest.Method = WebRequestMethods.Ftp.DeleteFile;                 //???
+
+        FtpWebResponse deleteResponse = (FtpWebResponse)ftpRequest.GetResponse();//data v requestu SMAŽE
+        Console.WriteLine("Delete File Complete. Status: " + deleteResponse.StatusDescription);
+
+        // Clean up
+        reader.Close();
+        responseStream.Close();
+        ftpResponse.Close();
+        uploadResponse.Close();
+        deleteResponse.Close();
+
+        Console.ReadLine();
+    }
+}
